c6e08997a7dbddc04f26b6c6663f4af421910119
zhenning-Zhu9 <2117802781@qq.com>
2026-05-29 17:11:34 +1200

    Fix MongoDB URI for Vercel build

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
 const client = new MongoClient(uri);
 
